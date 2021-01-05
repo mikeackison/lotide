ce2971979e5df2980b4052ade78b7efa39a44bab
Mike Ackison <mike.ackison@shopify.com>
2021-01-05 00:00:03 +0000

    add another test

--- tail.js
+++ tail.js
@@ -23,3 +23,7 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
+
+const test = tail(["Mike"]);
+assertEqual(test.length, 0)
+
