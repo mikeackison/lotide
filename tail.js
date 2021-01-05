f4e859713fd7561bece7b7316982512cce242447
Mike Ackison <mike.ackison@shopify.com>
2021-01-05 00:14:23 +0000

    check with eslint

--- tail.js
+++ tail.js
@@ -8,8 +8,8 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail  = function(array) {
-  return array.slice(1)
-}
+  return array.slice(1);
+};
 
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
@@ -18,14 +18,13 @@ assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 
-// Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
 
 const test = tail(["Mike"]);
-assertEqual(test.length, 0)
+assertEqual(test.length, 0);
 
 const test2 = tail([]);
-assertEqual(test2.length, 0)
+assertEqual(test2.length, 0);
