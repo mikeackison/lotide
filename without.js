f3f32646895a532d7963d797eb2f25c87293a814
Mike Ackison <mike.ackison@shopify.com>
2021-01-06 05:17:39 +0000

    first commit

---
+++ without.js
@@ -0,0 +1,135 @@
+const eqArrays = function(firstParam, secondParam) {
+  if (firstParam.length !== secondParam.length) {
+    return false;
+  }
+  for (let i = 0; i < firstParam.length; i++) {
+
+    if (firstParam[i] !== secondParam[i]) {
+      return false;
+    }
+
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅  Assertion Passed: ${actual} === ${expected} is true`);
+  } else {
+    console.log(`❌  Assertion Failed: ${actual} === ${expected} is false`);
+  }
+};
+
+
+const without = function(source, itemsToRemove) {
+
+  // accept two parameters souce array, item to remove array
+  // create an accumulator array, for the new array with only the remaining items, so the orignal is not changed
+  let newWithoutArray = [];
+
+  // loop thorough source array
+  for (let i = 0; i < source.length; i++) {
+    // console.log(typeof(source[i]))
+      
+
+    // loop though item to remove array
+    for (let j = 0; j < itemsToRemove.length; j++) {
+
+      // if element in source array is not in remove array, push them into newArray
+
+      
+      if ((source[i] !== itemsToRemove[j]) && (typeof(source[i]) == typeof(itemsToRemove[j]))) {
+        newWithoutArray.push(source[i]);
+      }
+    }
+  }
+  return newWithoutArray;
+
+};
+
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+console.log("Without function: ", without(words, ["lighthouse"]));
+
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+
+
+
+
+
+
+
+// const without = function(source, itemsToRemove) {
+
+//   // accept two parameters souce array, item to remove array
+//   // create an accumulator array, for the new array with only the remaining items, so the orignal is not changed
+//   let newWithoutArray = [];
+
+//   // loop thorough source array
+//   for (let i = 0; i < source.length; i++) {
+//     // console.log(typeof(source[i]))
+      
+
+//     // loop though item to remove array
+//     for (let j = 0; j < itemsToRemove.length; j++) {
+
+//       // if element in source array is not in remove array, push them into newArray
+
+//       if ((source[i] !== itemsToRemove[j]) && (typeof(source[i]) == typeof(itemsToRemove[j])) ) {
+//         newWithoutArray.push(source[i]);
+//       }
+//     }
+//   }
+//   return newWithoutArray;
+
+// };
+
+// const without = function(source, itemsToRemove) {
+
+//   // accept two parameters souce array, item to remove array
+//   // create an accumulator array, for the new array with only the remaining items, so the orignal is not changed
+//   let newWithoutArray = [];
+
+//   // loop thorough source array
+//   for (let i = 0; i < source.length; i++) {
+//     // console.log(typeof(source[i]))
+      
+
+//     // loop though item to remove array
+//     for (let j = 0; j < itemsToRemove.length; j++) {
+
+//       // if element in source array is not in remove array, push them into newArray
+
+//       console.log("source")
+//       console.log(source[i])
+
+//       console.log("itemsToRemove")
+//       console.log(itemsToRemove[j])
+
+//       console.log("newWithoutArray")
+//       console.log(newWithoutArray)
+
+      
+//       console.log("Source ", typeof(source[i]), source[i])
+      
+//       console.log("Remove ", typeof(itemsToRemove[j]), itemsToRemove[j])
+
+
+//       console.log("i = ", i)
+
+//       console.log("j = ", j)
+//       if ((source[i] !== itemsToRemove[j]) && (typeof(source[i]) == typeof(itemsToRemove[j])) ) {
+//         newWithoutArray.push(source[i]);
+//       }
+//     }
+//   }
+//   return newWithoutArray;
+
+// };
+
