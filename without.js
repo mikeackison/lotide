6c87c3c3ab2fea0491953854a5c66b55bb06a9e5
Mike Ackison <mike.ackison@shopify.com>
2021-01-06 14:40:41 +0000

    bug issues; add splice() to modify another array, to not modify the orignal

--- without.js
+++ without.js
@@ -26,20 +26,23 @@ const without = function(source, itemsToRemove) {
   // accept two parameters souce array, item to remove array
   // create an accumulator array, for the new array with only the remaining items, so the orignal is not changed
   let newWithoutArray = [];
-
+  
   // loop thorough source array
+  // push into another array to not modify the source
   for (let i = 0; i < source.length; i++) {
-    // console.log(typeof(source[i]))
-      
+    newWithoutArray.push(source[i]);
+  }
 
+  for (let i = 0; i < newWithoutArray.length; i++) {
+      
     // loop though item to remove array
     for (let j = 0; j < itemsToRemove.length; j++) {
 
-      // if element in source array is not in remove array, push them into newArray
-
-      
-      if ((source[i] !== itemsToRemove[j]) && (typeof(source[i]) == typeof(itemsToRemove[j]))) {
-        newWithoutArray.push(source[i]);
+      // if element in source array matches in remove array, splice them out of newArray using index
+    
+      if (newWithoutArray[i] === itemsToRemove[j]) {
+        newWithoutArray.splice(i, 1);
+        i--;
       }
     }
   }
@@ -47,19 +50,25 @@ const without = function(source, itemsToRemove) {
 
 };
 
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without([1, 2, "3"], [1, 2]), ["3"]);
 
 const words = ["hello", "world", "lighthouse"];
+console.log(words);
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
+console.log(words);
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 console.log("Without function: ", without(words, ["lighthouse"]));
 
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 
-
+assertArraysEqual(without([1, 2, 3], [2]), [1, 3]);
+assertArraysEqual(without([1, 2, "3"], [1, 2]), ["3"]);
+assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
 
 
 
