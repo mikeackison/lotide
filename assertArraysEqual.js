7d6451d195513c000e2bf2d4a7ce5bf540a05b50
Mike Ackison <mike.ackison@shopify.com>
2021-01-06 02:39:12 +0000

    first commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+
+
+const eqArrays = function(firstParam, secondParam) {
+  if (firstParam.length !== secondParam.length) {
+    return false;
+  }
+  for (let i = 0; i < firstParam.length; i++) {
+  
+    if (firstParam[i] === secondParam[i]) {
+      return true;
+    }
+    
+  return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅  Assertion Passed: ${actual} === ${expected} is true`);
+  } else {
+    console.log(`❌  Assertion Failed: ${actual} === ${expected} is false`);
+  }
+};
+
+
+assertArraysEqual(["Hello"], [1,2,3,4])
+assertArraysEqual(["Hello"], ["Hello"])
