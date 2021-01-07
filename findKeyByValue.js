37c129282e23c96ad66b1039ea4685212ec7a74c
Mike Ackison <mike.ackison@shopify.com>
2021-01-07 23:22:12 +0000

    just a save, no real changes

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -14,10 +14,11 @@ const findKeyByValue = function(object, value) {
   
   // iterate through and return the first key that returns the given value
   // genre is a placeholder - while using the for in loop we iterate through the objects keys
-  // object[genre] will return the values (names of shows) (genre the keys)
+  // object[genre] will return the values (names of shows) (genre the keys) could also use key as a placeholder
   for (const genre in object) {
 
     if (object[genre] === value) {
+      // console.log(genre, object[genre])
       return genre
     }
     
@@ -39,9 +40,7 @@ console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expance"), "comedy");
 
 
 // Implement the function findKeyByValue which takes in an object and a value. 
