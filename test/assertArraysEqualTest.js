d8bae4949d1b73cacf2fe6475fab822a9601c6f1
Mike Ackison <mike.ackison@shopify.com>
2021-01-11 21:36:47 +0000

    add chai, and found a bug

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,15 @@
+const assert = require('chai').assert;
 
 const assertArraysEqual = require('../assertArraysEqual');
 
 
-assertArraysEqual(["Hello"], [1,2,3,4]);
-assertArraysEqual(["Hello"], ["Hello"]);
+describe("assertArraysEqual", function () {
+  it('returns ["Hello"] for ["Hello"]', function() {
+    assert.strictEqual(assertArraysEqual(["Hello"], ["Hello"]));
+  });
+
+  it('returns [1, 2, 3] for [1, 2, 3]', function () {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  })
+
+});
