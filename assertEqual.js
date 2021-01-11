e301de0707ad46f6f3315a72cd436dac8bf29b1e
Mike Ackison <mike.ackison@shopify.com>
2021-01-11 18:42:32 +0000

    removed test files; file is now just funtion. use module exports funtion

--- assertEqual.js
+++ assertEqual.js
@@ -9,8 +9,6 @@ const assertEqual = function(actual, expected) {
 
 };
 
-assertEqual('"Lighthouse Labs"', '"Bootcamp"');
-assertEqual(1, 1);
-assertEqual('"Lighthouse Labs"', '"Lighthouse Labs"');
-assertEqual(1, '"1"');
-assertEqual(1, 2);
+module.exports = assertEqual;
+
+
