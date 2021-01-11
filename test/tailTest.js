ea6778a7f3a6591aa5c54faca3089168271a916f
Mike Ackison <mike.ackison@shopify.com>
2021-01-11 22:48:55 +0000

    test working; use deepEqual instead of strictEqaul

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,16 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
 const tail = require('../tail');
 
+describe("tail", function() {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', function() {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  });
 
+  it('returns [2, 3] for [1, 2, 3]', function () {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  });
 
-
-const test = tail(["Mike"]);
-assertEqual(test.length, 0);
-
-const test2 = tail([]);
-assertEqual(test2.length, 0);
+});
