0b4d60798d256d94ddc2a486d652f98f9abe138a
Mike Ackison <mike.ackison@shopify.com>
2021-01-11 19:40:47 +0000

    add test, use require to access files outside of directory

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+// const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+assertArraysEqual(["Hello"], [1,2,3,4]);
+assertArraysEqual(["Hello"], ["Hello"]);
