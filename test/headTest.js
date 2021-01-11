7a53537aaebe63a8dbc4079e06fb6674e646fa86
Mike Ackison <mike.ackison@shopify.com>
2021-01-11 22:07:00 +0000

    test passing

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,22 @@
-const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe("head", function () {
+  it("returns 1 for [1, 2, 3]", function() {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  
+  it("returns '5' for ['5']", function() {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  
+});
+
+
+
+// old code to be removed once all tests pass
+// assertEqual(head([5,6,7]), 5);
+// const assertEqual = require('../assertEqual');
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
