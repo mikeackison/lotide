cef88021df71fedee12d518f6270c7e4b28b7ce8
Mike Ackison <mike.ackison@shopify.com>
2021-01-12 00:36:52 +0000

    remove old assert tests, add module.exports

--- findKey.js
+++ findKey.js
@@ -1,15 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-    console.log(`❌  Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-
-};
-
-
 const findKey = function(object, callback) {
   // take in two paramenters, an object and a callback function
   // use the looping struckture from earlier
@@ -25,16 +13,4 @@ const findKey = function(object, callback) {
   // if no key matches the value return undefined
 };
 
-
-
-let results1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2); // => "noma"
-
-
-assertEqual(results1, "noma");
+module.exports = findKey;
