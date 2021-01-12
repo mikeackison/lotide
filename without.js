43d71dfed90d98825843c52966492246ba98f5f8
Mike Ackison <mike.ackison@shopify.com>
2021-01-12 00:46:32 +0000

    remove assert test, add module.exports

--- without.js
+++ without.js
@@ -1,26 +1,3 @@
-const eqArrays = function(firstParam, secondParam) {
-  if (firstParam.length !== secondParam.length) {
-    return false;
-  }
-  for (let i = 0; i < firstParam.length; i++) {
-
-    if (firstParam[i] !== secondParam[i]) {
-      return false;
-    }
-    
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅  Assertion Passed: ${actual} === ${expected} is true`);
-  } else {
-    console.log(`❌  Assertion Failed: ${actual} === ${expected} is false`);
-  }
-};
-
-
 const without = function(source, itemsToRemove) {
 
   // accept two parameters souce array, item to remove array
@@ -40,25 +17,5 @@ const without = function(source, itemsToRemove) {
 
 };
 
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-assertArraysEqual(without([1, 2, "3"], [1, 2]), ["3"]);
-
-const words = ["hello", "world", "lighthouse"];
-console.log(words);
-
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-console.log(words);
-
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-console.log("Without function: ", without(words, ["lighthouse"]));
-
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
-
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-
-assertArraysEqual(without([1, 2, 3], [2]), [1, 3]);
-assertArraysEqual(without([1, 2, "3"], [1, 2]), ["3"]);
-assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
+module.exports = without
 
