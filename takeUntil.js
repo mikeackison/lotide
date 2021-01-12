6cd0a4aef53f2dd5041b373179d356b75183d614
Mike Ackison <mike.ackison@shopify.com>
2021-01-12 00:44:49 +0000

    remove assert equal tests, add module.exports

--- takeUntil.js
+++ takeUntil.js
@@ -1,26 +1,3 @@
-const eqArrays = function(firstParam, secondParam) {
-  if (firstParam.length !== secondParam.length) {
-    return false;
-  }
-  for (let i = 0; i < firstParam.length; i++) {
-  
-    if (firstParam[i] !== secondParam[i]) {
-      return false;
-    }
-  
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅  Assertion Passed: ${actual} === ${expected} is true`);
-  } else {
-    console.log(`❌  Assertion Failed: ${actual} === ${expected} is false`);
-  }
-};
-
-
 const takeUntil = function(array, callback) {
   let newArray = []
   for (let word of array) {
@@ -34,6 +11,18 @@ const takeUntil = function(array, callback) {
   return newArray;
 }
 
+// Expected Input
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
+module.exports = takeUntil
 
 
 
@@ -49,16 +38,4 @@ const takeUntil = function(array, callback) {
 
 
 
-// Expected Input
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
 
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-assertArraysEqual(results2,  ['I\'ve', 'been', 'to', 'Hollywood' ]);
