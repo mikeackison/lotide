dc2e75f14c941647ab75e7dd120dee36fa881aed
Mike Ackison <mike.ackison@shopify.com>
2021-01-12 00:40:02 +0000

    remove old asser equals tests, ass module.export

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-    console.log(`❌  Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-
-};
 
 const findKeyByValue = function(object, value) {
   // take in an object and a value
@@ -27,21 +17,7 @@ const findKeyByValue = function(object, value) {
   // if no key matches the value return undefined
 }
 
-
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-
+module.exports = findKeyByValue
 
 // Implement the function findKeyByValue which takes in an object and a value. 
 // It should scan the object and return the first key which contains the given value.
